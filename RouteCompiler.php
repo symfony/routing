sudent/symfony

[Routing] added hostname matching support to RouteCompiler

src/Symfony/Component/Routing/RouteCompiler.php
@@ -29,19 +29,75 @@ class RouteCompiler implements RouteCompilerInterface
      */
     public function compile(Route $route)
     {
+        $staticPrefix = null;
+        $hostnameVariables = array();
+        $pathVariables = array();
+        $variables = array();
+        $tokens = array();
+        $regex = null;
+        $hostnameRegex = null;
+        $hostnameTokens = array();
+
+        if (null !== $hostnamePattern = $route->getHostnamePattern()) {
+
+            $result = $this->compilePattern($route, $hostnamePattern, false);
+
+            $hostnameVariables = $result['variables'];
+            $variables = array_merge($variables, $hostnameVariables);
+
+            $hostnameTokens = $result['tokens'];
+            $hostnameRegex = $result['regex'];
+        }
+
         $pattern = $route->getPattern();
+        $result = $this->compilePattern($route, $pattern, true);
+
+        $staticPrefix = $result['staticPrefix'];
+
+        $pathVariables = $result['variables'];
+        $variables = array_merge($variables, $pathVariables);
+
+        $tokens = $result['tokens'];
+        $regex = $result['regex'];
+
+        return new CompiledRoute(
+            $route,
+            $staticPrefix,
+            $regex,
+            $tokens,
+            array_unique($variables),
+            $pathVariables,
+            $hostnameVariables,
+            $hostnameRegex,
+            $hostnameTokens
+        );
+    }
+
+    private function compilePattern(Route $route, $pattern, $isPath)
+    {
         $len = strlen($pattern);
         $tokens = array();
         $variables = array();
         $pos = 0;
-        preg_match_all('#.\{([\w\d_]+)\}#', $pattern, $matches, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
+
+        if ($isPath) {
+            $re = '#(?P<char>.)\{(?P<var>[\w\d_]+)\}#';
+        } else {
+            $re = '#(?P<char>^|.)\{(?P<var>[\w\d_]+)\}#';
+        }
+
+        preg_match_all($re, $pattern, $matches, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
         foreach ($matches as $match) {
             if ($text = substr($pattern, $pos, $match[0][1] - $pos)) {
                 $tokens[] = array('text', $text);
             }
-            $seps = array($pattern[$pos]);
+            if ($isPath) {
+                $seps = array($pattern[$pos]);
+            } else {
+                $seps = array('.');
+            }
             $pos = $match[0][1] + strlen($match[0][0]);
-            $var = $match[1][0];
+            $var = $match['var'][0];
 
             if ($req = $route->getRequirement($var)) {
                 $regexp = $req;
@@ -52,7 +108,7 @@ class RouteCompiler implements RouteCompilerInterface
                 $regexp = sprintf('[^%s]+?', preg_quote(implode('', array_unique($seps)), self::REGEX_DELIMITER));
             }
 
-            $tokens[] = array('variable', $match[0][0][0], $regexp, $var);
+            $tokens[] = array('variable', $match['char'][0], $regexp, $var);
 
             if (in_array($var, $variables)) {
                 throw new \LogicException(sprintf('Route pattern "%s" cannot reference variable name "%s" more than once.', $route->getPattern(), $var));
@@ -67,12 +123,14 @@ class RouteCompiler implements RouteCompilerInterface
 
         // find the first optional token
         $firstOptional = INF;
-        for ($i = count($tokens) - 1; $i >= 0; $i--) {
-            $token = $tokens[$i];
-            if ('variable' === $token[0] && $route->hasDefault($token[3])) {
-                $firstOptional = $i;
-            } else {
-                break;
+        if ($isPath) {
+            for ($i = count($tokens) - 1; $i >= 0; $i--) {
+                $token = $tokens[$i];
+                if ('variable' === $token[0] && $route->hasDefault($token[3])) {
+                    $firstOptional = $i;
+                } else {
+                    break;
+                }
             }
         }
 
@@ -82,12 +140,11 @@ class RouteCompiler implements RouteCompilerInterface
             $regexp .= $this->computeRegexp($tokens, $i, $firstOptional);
         }
 
-        return new CompiledRoute(
-            $route,
-            'text' === $tokens[0][0] ? $tokens[0][1] : '',
-            self::REGEX_DELIMITER.'^'.$regexp.'$'.self::REGEX_DELIMITER.'s',
-            array_reverse($tokens),
-            $variables
+        return array(
+            'staticPrefix' => 'text' === $tokens[0][0] ? $tokens[0][1] : '',
+            'regex' => self::REGEX_DELIMITER.'^'.$regexp.'$'.self::REGEX_DELIMITER.'s',
+            'tokens' => array_reverse($tokens),
+            'variables' => $variables,
         );
     }
 

symfony/routing

RouteCompiler.php
@@ -37,11 +37,58 @@ class RouteCompiler implements RouteCompilerInterface
      */
     public function compile(Route $route)
     {
+        $staticPrefix = null;
+        $hostnameVariables = array();
+        $pathVariables = array();
+        $variables = array();
+        $tokens = array();
+        $regex = null;
+        $hostnameRegex = null;
+        $hostnameTokens = array();
+
+        if (null !== $hostnamePattern = $route->getHostnamePattern()) {
+
+            $result = $this->compilePattern($route, $hostnamePattern, true);
+
+            $hostnameVariables = $result['variables'];
+            $variables = array_merge($variables, $hostnameVariables);
+
+            $hostnameTokens = $result['tokens'];
+            $hostnameRegex = $result['regex'];
+        }
+
         $pattern = $route->getPattern();
+
+        $result = $this->compilePattern($route, $pattern, false);
+
+        $staticPrefix = $result['staticPrefix'];
+
+        $pathVariables = $result['variables'];
+        $variables = array_merge($variables, $pathVariables);
+
+        $tokens = $result['tokens'];
+        $regex = $result['regex'];
+
+        return new CompiledRoute(
+            $staticPrefix,
+            $regex,
+            $tokens,
+            array_unique($variables),
+            $pathVariables,
+            $hostnameVariables,
+            $hostnameRegex,
+            $hostnameTokens
+        );
+    }
+
+    private function compilePattern(Route $route, $pattern, $isHostname)
+    {
+        $len = strlen($pattern);
         $tokens = array();
         $variables = array();
         $matches = array();
         $pos = 0;
+        $defaultSeparator = $isHostname ? '.' : '/';
 
         // Match all variables enclosed in "{}" and iterate over them. But we only want to match the innermost variable
         // in case of nested "{}", e.g. {foo{bar}}. This in ensured because \w does not match "{" or "}" itself.
@@ -78,7 +125,11 @@ class RouteCompiler implements RouteCompilerInterface
                 // Also even if {_format} was not optional the requirement prevents that {page} matches something that was originally
                 // part of {_format} when generating the URL, e.g. _format = 'mobile.html'.
                 $nextSeparator = $this->findNextSeparator($followingPattern);
-                $regexp = sprintf('[^/%s]+', '/' !== $nextSeparator && '' !== $nextSeparator ? preg_quote($nextSeparator, self::REGEX_DELIMITER) : '');
+                $regexp = sprintf(
+                    '[^%s%s]+',
+                    preg_quote($defaultSeparator, self::REGEX_DELIMITER),
+                    $defaultSeparator !== $nextSeparator && '' !== $nextSeparator ? preg_quote($nextSeparator, self::REGEX_DELIMITER) : ''
+                );
                 if (('' !== $nextSeparator && !preg_match('#^\{\w+\}#', $followingPattern)) || '' === $followingPattern) {
                     // When we have a separator, which is disallowed for the variable, we can optimize the regex with a possessive
                     // quantifier. This prevents useless backtracking of PCRE and improves performance by 20% for matching those patterns.
@@ -99,12 +150,14 @@ class RouteCompiler implements RouteCompilerInterface
 
         // find the first optional token
         $firstOptional = INF;
-        for ($i = count($tokens) - 1; $i >= 0; $i--) {
-            $token = $tokens[$i];
-            if ('variable' === $token[0] && $route->hasDefault($token[3])) {
-                $firstOptional = $i;
-            } else {
-                break;
+        if (!$isHostname) {
+            for ($i = count($tokens) - 1; $i >= 0; $i--) {
+                $token = $tokens[$i];
+                if ('variable' === $token[0] && $route->hasDefault($token[3])) {
+                    $firstOptional = $i;
+                } else {
+                    break;
+                }
             }
         }
 
@@ -114,11 +167,11 @@ class RouteCompiler implements RouteCompilerInterface
             $regexp .= $this->computeRegexp($tokens, $i, $firstOptional);
         }
 
-        return new CompiledRoute(
-            'text' === $tokens[0][0] ? $tokens[0][1] : '',
-            self::REGEX_DELIMITER.'^'.$regexp.'$'.self::REGEX_DELIMITER.'s',
-            array_reverse($tokens),
-            $variables
+        return array(
+            'staticPrefix' => 'text' === $tokens[0][0] ? $tokens[0][1] : '',
+            'regex' => self::REGEX_DELIMITER.'^'.$regexp.'$'.self::REGEX_DELIMITER.'s',
+            'tokens' => array_reverse($tokens),
+            'variables' => $variables,
         );
     }
 