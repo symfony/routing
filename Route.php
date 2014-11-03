5b6193a36bfadd8add9e723fc52bfcdaeb208d4d
Dries Vints <dries.vints@gmail.com>
2014-11-03 14:22:55 +0100

    Remove merge conflict

--- Route.php
+++ Route.php
@@ -61,6 +61,9 @@ class Route implements \Serializable
      */
     private $compiled;
 
+    /**
+     * @var string
+     */
     private $condition;
 
     /**
@@ -130,13 +133,11 @@ class Route implements \Serializable
         $this->options = $data['options'];
         $this->schemes = $data['schemes'];
         $this->methods = $data['methods'];
-<<<<<<< HEAD
         $this->condition = $data['condition'];
-=======
+
         if (isset($data['compiled'])) {
             $this->compiled = $data['compiled'];
         }
->>>>>>> 2.3
     }
 
     /**
