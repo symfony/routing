b7319099e64228b69fcd53822f44e79512c46390
Piotr Ossowski <piooss1@gmail.com>
2021-01-24 11:51:51 +0100

    Update Route.php
    Fix typo

--- Route.php
+++ Route.php
@@ -39,7 +39,7 @@ class Route implements \Serializable
      * Available options:
      *
      *  * compiler_class: A class name able to compile this route instance (RouteCompiler by default)
-     *  * utf8:           Whether UTF-8 matching is enforced ot not
+     *  * utf8:           Whether UTF-8 matching is enforced or not
      *
      * @param string          $path         The path pattern to match
      * @param array           $defaults     An array of default parameter values
